5c5922b2c0aeaef78b9b086609c8fd14fb65f1d9
ovis1me1me <126409777+ovis1me1me@users.noreply.github.com>
2023-05-16 12:45:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
-
-x=st.slider('Select a value')
-st.write(x, 'squared is', x*x)
+import pandas ad pd
+import numpy as np
+import random
+df=pd.DataFrame(np.random.randn(500,2)/[50,50]+[37.76,-122.4],columns=['lat,'lon'])
+st.map(df)                                                                     
+#x=st.slider('Select a value')
+#st.write(x, 'squared is', x*x)
